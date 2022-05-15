c112b7efc098647daf07ddfda3a27e0182d0a805
veveLP <lukasekhouzar@seznam.cz>
2022-05-16 00:38:49 +0200

    v1

--- WindowsFormsApp55/Form1.Designer.cs
+++ WindowsFormsApp55/Form1.Designer.cs
@@ -28,13 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(41, 15);
+            this.textBox1.MaxLength = 63;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(41, 38);
+            this.textBox2.MaxLength = 15;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(132, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(41, 64);
+            this.textBox3.MaxLength = 15;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(132, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "2:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "10:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "16:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(189, 96);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Convertor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- WindowsFormsApp55/Form1.cs
+++ WindowsFormsApp55/Form1.cs
@@ -14,7 +14,98 @@ namespace WindowsFormsApp55
     {
         public Form1()
         {
+            /*Navrhněte metodu BinToDec s jedním parametrem typu string, která převede binární
+            číslo ve tvaru řetězce znaků na číslo desítkové.
+            Navrhněte metodu DecToBin s jedním parametrem typu celé číslo, která převede
+            desítkové číslo na číslo binární ve tvaru řetězce znaků.
+            Navrhněte metodu HexToDec s jedním parametrem typu String, která převede
+            šestnáctkové číslo ve tvaru řetězce znaků na číslo desítkové.
+            Navrhněte metodu DecToHex s jedním parametrem typu celé číslo, která převede
+            desítkové číslo na číslo šestnáctkové ve tvaru řetězce znaků.
+            Navrhněte metodu HexToBin s jedním parametrem typu String, která převede
+            šestnáctkové číslo ve tvaru řetězce znaků na číslo binární ve tvaru řetězce znaků.
+            Navrhněte metodu BinToHex s jedním parametrem typu String, která převede binární
+            číslo ve tvaru řetězce znaků na číslo šestnáctkové ve tvaru řetězce znaků.
+            Vytvořte program, který bude mít 3 TextBoxy, pro zápis čísla v dané soustavě
+            (2,10,16). Uživatel zapíše číslo do jednoho z TexBoxů a do ostatních TexBoxů se číslo přepočítá.
+            Do každého TextBoxu umožněte zapisovat pouze přípustné znaky dané soustavy.
+
+            Odevzdejte odkaz na GitHub repozitář.*/
             InitializeComponent();
+        }
+
+        public string BinToDec(string txt)
+        {
+            if(txt == "") { return ""; }
+            return Convert.ToInt64(txt, 2).ToString();
+        }
+
+        public string BinToHex(string txt)
+        {
+            return DecToHex(BinToDec(txt));
+        }
+
+        public string DecToBin(string txt) {
+            long numb;
+            try
+            {
+                numb = Int64.Parse(txt);
+            }
+            catch (Exception) { return ""; }
+            return Convert.ToString(numb, 2); }
+
+        public string DecToHex(string txt) {
+            long numb;
+            try
+            {
+                numb = Int64.Parse(txt);
+            }
+            catch (Exception) { return ""; }
+            return numb.ToString("X"); }
+
+        public string HexToDec(string txt)
+        {
+            if (txt == "") { return ""; }
+            return Convert.ToInt64(txt, 16).ToString();
+        }
+
+        public string HexToBin(String txt)
+        {
+            if (txt == "") { return ""; }
+            return DecToBin(HexToDec(txt));
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.Text = BinToDec(textBox1.Text);
+            textBox3.Text = BinToHex(textBox1.Text);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = DecToBin(textBox2.Text);
+            textBox3.Text = DecToHex(textBox2.Text);
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.Text = HexToDec(textBox3.Text);
+            textBox1.Text = HexToBin(textBox3.Text);
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !("\b01".Contains(e.KeyChar));
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !("\b0123456789".Contains(e.KeyChar));
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !("\b0123456789ABCDEF".Contains(e.KeyChar));
         }
     }
 }
